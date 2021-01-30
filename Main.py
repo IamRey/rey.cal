f2c41f2e6a7066ba7ab584ebd97e4d76138d53af
IamRey <78278799+IamRey@users.noreply.github.com>
2021-01-31 02:21:27 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+print("Hi I Am Rey\n")
+print("Simple Calculator")
+n1=float(input ())
+n2=float(input ())
+cal=input ()
+if cal=="+":
+print(n1+n2)
+elif cal=="-":
+print(n1-n2)
+elif cal=="*":
+print(n1*n2)
+elif cal=="/":
+print(n1/n2)
+elif cal=="%":
+print(n1%n2)
+else:
+print("Illogical Inputs!!")
